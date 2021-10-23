0255957a85c3a27fad649cb4c49f20a82376877f
Jake <xxjakexx14@gmail.com>
2021-10-23 14:00:20 -0400

    added Employee object tests

---
+++ __test__/Employee.test.js
@@ -0,0 +1,28 @@
+const Employee = require("../lib/Employee");
+
+test("creates an employee object", () => {
+  const employee = new Employee("Dave", "email@email.com", 1);
+
+  expect(employee.name).toBe("Dave");
+  expect(employee.email).toBe("email@email.com");
+  expect(employee.id).toEqual(expect.any(Number));
+  console.log(employee);
+});
+
+test("get employees name value", () => {
+  const employee = new Employee("Dave", "email@email.com", 1);
+
+  expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+});
+
+test("get employees email value", () => {
+  const employee = new Employee("Dave", "email@email.com", 1);
+
+  expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+});
+
+test("get employees id value", () => {
+  const employee = new Employee("Dave", "email@email.com", 1);
+
+  expect(employee.getId()).toEqual(employee.id);
+});
